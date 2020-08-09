70d6ce194641ec61145b394223c0a874caa51f91
gilbertttsubay <35297439+gilbertttsubay@users.noreply.github.com>
2020-08-09 19:57:36 +0700

    Update main.go
    tambahin fungsi menu

--- project01/client/main.go
+++ project01/client/main.go
@@ -123,3 +123,102 @@ func Logout(conn model.InventoryClient) string {
     return "Kesalahan Pada sistem"
   }
 }
+
+
+
+//Punya Gilbert Subay
+
+
+func addItem (conn model.InventoryClient) {
+
+  var (
+    idItem int32
+    namaItem string
+    jumlah int32
+    kategori int32
+    idUser int32 = config.IdUser
+  )
+
+  fmt.Println("Masukkan idItem")
+  fmt.Scan(&idItem)
+
+  fmt.Println("Masukkan nama item")
+  fmt.Scan(&namaItem)
+
+  fmt.Println("Masukkan jumlah")
+  fmt.Scan(&jumlah)
+
+  fmt.Println("Masukkan kategori")
+  fmt.Scan(&kategori)
+
+  req:= &model.Item{
+    IdItem : idItem,
+    NamaItem: namaItem,
+    Jumlah: jumlah,
+    Kategori: kategori,
+    IdUser: idUser,
+  }
+
+  res,err:= conn.AddItem(context.Background(), req)
+
+  if err != nil {
+    log.Fatalf("Tidak bisa menerima response terkait Add item", err)
+  }
+
+  fmt.Println("Status Anda adalah ", res.GetStatus())
+  fmt.Println("Message Anda adalah ", res.GetMessage())
+}
+
+func ambilItem(conn model.InventoryClient){
+  var (
+    idItem int32
+  )
+
+  fmt.Println("Masukkan id item")
+  fmt.Scan(&idItem)
+
+  req:= &model.Item{
+    IdItem: idItem,
+  }
+
+  res,err:= conn.GetItem(context.Background(),req)
+
+  if err != nil {
+    log.Fatalf("Tidak bisa menerima response terkait Get Item", err)
+  }
+
+  fmt.Println("Item Anda adalah ", res)
+}
+
+func showPerItem(conn model.InventoryClient){
+  var idItem int32
+  
+  fmt.Println("Masukkan id item")
+  fmt.Scan(&idItem)
+
+  req:= &model.Item{
+    IdItem:idItem,
+  }
+
+  res,err := conn.Show(context.Background(),req)
+
+  if err != nil {
+    log.Fatalf("Tidak bisa menerima response terkait Show", err)
+  }
+
+  fmt.Println("Item yang Anda cari Adalah ", res)
+}
+
+func ShowAll(conn model.InventoryClient) {
+  req:= &model.Item{
+    IdUser : config.Id_user,
+  }
+
+  res, err:= conn.ShowAll(context.Background(), req)
+
+  if err != nil {
+    log.Fatalf("Tidak bisa menerima response terkait Show All", err)
+  }
+
+  fmt.Println("List item Anda adalah ", res)
+}
